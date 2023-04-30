6d6bc9ef711973dc5fed96b18206177a80494416
Haseeb <haseeb_heaven@yahoo.com>
2023-04-30 22:08:47 +0530

    Added Save Code feature

--- LangChain-Coder.py
+++ LangChain-Coder.py
@@ -18,6 +18,7 @@ from langchain.llms import OpenAI as LangChainOpenAI
 import openai
 from dotenv import load_dotenv
 
+# Load the environment variables
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -27,37 +28,41 @@ code_prompt = st.text_input("Enter a prompt to generate the code")
 code_language = st.selectbox("Select the language", [
                              "C", "Cpp", "Python", "Javascript"])
 
-# Buttons
+# Generate and Run Buttons
 button_generate = st.button("Generate Code")
 button_run = st.button("Run Code")
-button_generate_run = st.button("Generate and Run Code")
+code_file = st.text_input("Enter file name:")
+button_save = st.button("Save Code")
 
 # Prompt Templates
 code_template = PromptTemplate(
-    input_variables=['topic'],
-    template='Write me code in ' + f'{code_language} language' + ' for {topic}'
+    input_variables=['code_topic'],
+    template='Write me code in ' +
+    f'{code_language} language' + ' for {code_topic}'
 )
 
 code_fix_template = PromptTemplate(
-    input_variables=['topic'],
-    template='Fix any error in the following code in ' + f'{code_language} language' + ' for {topic}'
+    input_variables=['code_topic'],
+    template='Fix any error in the following code in ' +
+    f'{code_language} language' + ' for {code_topic}'
 )
 
 # Memory for the conversation
-memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+memory = ConversationBufferMemory(
+    input_key='code_topic', memory_key='chat_history')
 
 # LLM Chains definition
 # Create an OpenAI LLM model
 open_ai_llm = OpenAI(temperature=0.7, max_tokens=1000)
 
 # Create a chain that generates the code
-code_chain = LLMChain(llm=open_ai_llm, prompt=code_template, output_key='code', memory=memory, verbose=True)
+code_chain = LLMChain(llm=open_ai_llm, prompt=code_template,output_key='code', memory=memory, verbose=True)
 
 # Create a chain that fixes the code
-code_fix_chain = LLMChain(llm=open_ai_llm, prompt=code_fix_template, output_key='code_fix', memory=memory,verbose=True)
+code_fix_chain = LLMChain(llm=open_ai_llm, prompt=code_fix_template,output_key='code_fix', memory=memory, verbose=True)
 
 # Create a sequential chain that combines the two chains above
-sequential_chain = SequentialChain(chains=[code_chain, code_fix_chain],input_variables=['topic'],output_variables=['code', 'code_fix'])
+sequential_chain = SequentialChain(chains=[code_chain, code_fix_chain], input_variables=['code_topic'], output_variables=['code', 'code_fix'])
 
 global generated_code
 
@@ -103,21 +108,21 @@ class PythonREPL:
             output = str(e)
         return output
 
-
+# Define the Run query function
 def run_query(query, model_kwargs, max_iterations):
     # Create a LangChainOpenAI object
     llm = LangChainOpenAI(**model_kwargs)
     # Create the python REPL tool
-    python_repl = lc_agents.Tool("Python REPL", PythonREPL().run,
-                                "A Python shell. Use this to execute python commands.")
+    python_repl = lc_agents.Tool("Python REPL", PythonREPL().run,"A Python shell. Use this to execute python commands.")
     # Create a list of tools
     tools = [python_repl]
     # Initialize the agent
     agent = lc_agents.initialize_agent(tools, llm, agent=lc_agents.AgentType.ZERO_SHOT_REACT_DESCRIPTION,model_kwargs=model_kwargs, verbose=True, max_iterations=max_iterations)
     # Run the agent
     response = agent.run(query)
+    return response
 
-
+# Define the Run code function
 def run_code(code, language):
     logger = logging.getLogger(__name__)
     logger.info(f"Running code: {code} in language: {language}")
@@ -178,12 +183,13 @@ if "code_language" not in st.session_state:
     st.session_state.code_language = ""
 
 # Generate the code
-if button_generate or button_generate_run and code_prompt:
+if button_generate and code_prompt:
     logger = logging.getLogger(__name__)
     try:
         st.session_state.generated_code = code_chain.run(code_prompt)
         st.session_state.code_language = code_language
-        st.code(st.session_state.generated_code,language=st.session_state.code_language.lower())
+        st.code(st.session_state.generated_code,
+                language=st.session_state.code_language.lower())
 
         with st.expander('Message History'):
             st.info(memory.buffer)
@@ -191,8 +197,25 @@ if button_generate or button_generate_run and code_prompt:
         st.write(traceback.format_exc())
         logger.error(f"Error in code generation: {traceback.format_exc()}")
 
+# Save the code to a file
+if button_save and st.session_state.generated_code:
+    logger = logging.getLogger(__name__)
+    try:
+        file_name = code_file
+        logger.info(f"Saving code to file: {file_name}")
+        if file_name:
+            with open(file_name, "w") as f:
+                f.write(st.session_state.generated_code)
+            st.success(f"Code saved to file {file_name}")
+            logger.info(f"Code saved to file {file_name}")
+        st.code(st.session_state.generated_code,language=st.session_state.code_language.lower())
+    except Exception as e:
+        st.write(traceback.format_exc())
+        logger.error(f"Error in code saving: {traceback.format_exc()}")
+
+
 # Execute the code
-if button_run or button_generate_run and code_prompt:
+if button_run and code_prompt:
     logger = logging.getLogger(__name__)
     try:
         logger.info(f"Running code: {st.session_state.generated_code} in language: {st.session_state.code_language}")
@@ -200,9 +223,10 @@ if button_run or button_generate_run and code_prompt:
         logger.info(f"Output execution: {output}")
 
         if "error" in output.lower() or "exception" in output.lower() or "SyntaxError" in output.lower() or "NameError" in output.lower():
-                
+
             logger.error(f"Error in code execution: {output}")
-            response = sequential_chain({'topic': st.session_state.generated_code})
+            response = sequential_chain(
+                {'code_topic': st.session_state.generated_code})
             fixed_code = response['code_fix']
             st.code(fixed_code, language=st.session_state.code_language.lower())
 
@@ -211,8 +235,8 @@ if button_run or button_generate_run and code_prompt:
             logger.warning(f"Trying to run fixed code: {fixed_code}")
             output = run_code(fixed_code, st.session_state.code_language)
             logger.warning(f"Fixed code output: {output}")
-        if not button_generate_run:
-            st.code(st.session_state.generated_code,language=st.session_state.code_language.lower())
+
+        st.code(st.session_state.generated_code,language=st.session_state.code_language.lower())
         st.write("Execution Output:")
         st.write(output)
         logger.info(f"Execution Output: {output}")
